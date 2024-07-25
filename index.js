d5d5069e79eb42a09648c5a556df79b607208ad3
nontakorn <nontakorn_chris@hotmail.com>
2024-07-25 11:07:47 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+    const p = document.getElementsByTagName('text');
+    text.textContent = "This is really cool!";
+    console.log(text)
+})
